a31c9287642eefdf8573bfc0439da02d9ac0d7e6
IMAN <emanvbdallah7@gmail.com>
2026-03-27 22:52:38 +0200

    Add files via upload

---
+++ routes/brand.routes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  createBrand,
+  getBrands,
+  getBrand,
+  updateBrand,
+  deleteBrand,
+} = require("../controllers/brand.controller");
+
+router.post("/", createBrand);
+router.get("/", getBrands);
+router.get("/:id", getBrand);
+router.put("/:id", updateBrand);
+router.delete("/:id", deleteBrand);
+
+module.exports = router;

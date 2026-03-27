fbda5ad1414eed41fcaf66d4531ed966c051a732
IMAN <emanvbdallah7@gmail.com>
2026-03-27 22:54:12 +0200

    Add files via upload

---
+++ models/brand.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const brandSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: 3,
+      maxlength: 32,
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    image: String,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Brand", brandSchema);
